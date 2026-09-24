d3ec70cceea466f2078ec156adff79a0fbee52d7
Syldel <nightfand@hotmail.com>
2026-09-24 22:28:16 +0200

    chore(release): 0.22.0

--- src/package-version.ts
+++ src/package-version.ts
@@ -16,4 +16,4 @@
  * Voir `docs/trading/mobile-app-integration.md` dans nest-trading-bot.
  * ============================================================================
  */
-export const PACKAGE_VERSION = '0.21.0';
+export const PACKAGE_VERSION = '0.22.0';
